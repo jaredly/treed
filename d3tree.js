3c8e739975829198525de26efae0027a2dc14640
Jared Forsyth <jared@jaredforsyth.com>
2014-04-08 23:17:29 -0600

    editing example doc

--- d3tree.js
+++ d3tree.js
@@ -2,7 +2,7 @@
 var margin = {top: 20, right: 120, bottom: 20, left: 120},
     width = 960 - margin.right - margin.left,
     height = 800 - margin.top - margin.bottom;
-    
+
 var i = 0,
     duration = 750,
     root;
@@ -19,7 +19,7 @@ var tree = d3.layout.tree()
 var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.y, d.x]; });
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#d3view").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
