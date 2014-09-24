ec14cc974b340966f683fb9bb1e938d5cd0cbcbd
Jared Forsyth <jared@jaredforsyth.com>
2014-09-24 10:03:27 -0600

    work toward breadcrumb

--- rx/plugins/rebase/index.js
+++ rx/plugins/rebase/index.js
@@ -1,7 +1,17 @@
 
+var Breadcrumb = require('./breadcrumb')
+
 module.exports = {
   store: require('./store'),
   node: require('./node'),
   keys: require('./keys'),
+
+  view: {
+    blocks: {
+      top: function (actions, state, store) {
+        return Breadcrumb({store: store})
+      },
+    },
+  },
 }
 
