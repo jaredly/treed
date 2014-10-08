db8589a2a98479e2aa60d62e93bcb0a54c98881a
Jared Forsyth <jared@jaredforsyth.com>
2014-10-07 18:43:29 -0600

    ensure in view with some padding now

--- rx/util/ensure-in-view.js
+++ rx/util/ensure-in-view.js
@@ -11,9 +11,14 @@ function ensureInView(item) {
     st = window.getComputedStyle(parent)
   }
   var pox = parent.getBoundingClientRect()
-  if (bb.top < pox.top) return item.scrollIntoView()
-  if (bb.bottom > pox.bottom) {
-    item.scrollIntoView(false)
+  if (bb.top < pox.top + 100) {
+    parent.scrollTop -= pox.top - bb.top + 100
+    return
+    // return item.scrollIntoView()
+  }
+  if (bb.bottom > pox.bottom - 100) {
+    // item.scrollIntoView(false)
+    parent.scrollTop += bb.bottom - pox.bottom + 100
   }
 }
 
